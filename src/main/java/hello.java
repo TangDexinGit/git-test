abb5a7a9c84163ab740050fc3fcc28edcb1a3272
TangDx <2500140258@qq.com>
2022-05-10 21:37:15 +0800

    提交代码

---
+++ src/main/java/hello.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassName: hello
+ * @Description:
+ * @Author: TangDx
+ * @DATE: 2022/5/10 21:35
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
